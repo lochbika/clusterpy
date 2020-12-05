422c0d558b4b8691744b472ff9b665510c32f16b
Kai <kai_lochi@gmx.de>
2020-12-05 17:44:53 +0100

    pep requirements

--- example_use.py
+++ example_use.py
@@ -12,5 +12,4 @@ t = 0
 tslice = ds_disk.isel(time=t, x=range(ds_disk.sizes['x']), y=range(ds_disk.sizes['y'])).to_array()
 cldata = xr.where(tslice > -999999999, -1, -1)
 
-cells = cluster.ClusterArray(tslice,thres).get_clusterarray()
-
+cells = cluster.ClusterArray(tslice, thres).get_clusterarray()
