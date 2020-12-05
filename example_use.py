943d3621ccb6759b8421139a46b081a019fa4d48
Kai <kai_lochi@gmx.de>
2020-12-05 17:34:02 +0100

    added needed fortran source code and a basic python script that demonstrates the use

---
+++ example_use.py
@@ -0,0 +1,16 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+import numpy as np
+from clusterpy import cluster
+
+infile = '/home/kai/PycharmProjects/clusterpy/cape.surfprec.100_coarse.nc'
+thres = 0.6/3600
+ 
+ds_disk = xr.open_dataset(infile, decode_times=False)
+
+t = 0
+tslice = ds_disk.isel(time=t, x=range(ds_disk.sizes['x']), y=range(ds_disk.sizes['y'])).to_array()
+cldata = xr.where(tslice > -999999999, -1, -1)
+
+cells = cluster.clusterarray(tslice,thres).get_clusterarray()
+
